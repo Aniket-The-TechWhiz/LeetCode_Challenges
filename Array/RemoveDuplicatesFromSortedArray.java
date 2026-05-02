c163ed37602888ce569faf43424296e954175248
Aniket-The-TechWhiz <aniketyelameli@gmail.com>
2026-05-02 13:15:16 +0530

    updated

---
+++ Array/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,31 @@
+class RemoveDuplicatesFromSortedArray {
+
+    public static void main(String args[]) {
+
+        int arr[] = {1,2,2,3};
+        int n = arr.length;
+
+        for(int i = 0; i < n; i++) {
+
+            int j = i + 1;
+
+            while(j < n) {
+
+                if(arr[i] == arr[j]) {
+                    for(int k = j; k < n - 1; k++) {
+                        arr[k] = arr[k + 1];
+                    }
+
+                    n--;
+
+                } else {
+                    j++;
+                }
+            }
+        }
+
+        for(int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
--- Array/Solution2.java
+++ Array/Solution2.java
@@ -1,30 +1,37 @@
 package leetcode.Array;
 
-class Solution2{
-    public static void main(String args[])
-    {
-        double d=findMedianSortedArrays(new int[]{1,23,3}, new int[]{2,4,3});
+import java.util.Arrays;
+
+class Solution2 {
+    public static void main(String args[]) {
+        double d = findMedianSortedArrays(new int[]{1,23,3}, new int[]{2,4,3});
+        System.out.println("\nMedian: " + d);
     }
 
     public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
-       int[] temp = new int[nums1.length + nums2.length];
-       for(int i=0;i<nums1.length;i++)
-       {
-        temp[i]=nums1[i];
-       }
-       for(int i=0;i<nums2.length;i++)
-       {
-       temp[nums1.length + i] = nums2[i];
-       }
-       for(int i=0;i<temp.length;i++)
-       {
-        System.out.print(temp[i]+" ");
-       }
-       int m=temp.length/2;
-       if(temp.length%2==0)
-       {
-        return
-       }
-       return 1.23;
+        int[] temp = new int[nums1.length + nums2.length];
+
+        for (int i = 0; i < nums1.length; i++) {
+            temp[i] = nums1[i];
+        }
+
+        for (int i = 0; i < nums2.length; i++) {
+            temp[nums1.length + i] = nums2[i];
+        }
+
+        // Sort before finding median
+        Arrays.sort(temp);
+
+        for (int i = 0; i < temp.length; i++) {
+            System.out.print(temp[i] + " ");
+        }
+
+        int m = temp.length / 2;
+
+        if (temp.length % 2 == 0) {
+            return (temp[m - 1] + temp[m]) / 2.0;
+        }
+
+        return temp[m];
     }
 }
